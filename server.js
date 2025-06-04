f6a203b12d0bdbcca0d241d8844f5c332791edf4
Niche-Apps <144236363+Niche-Apps@users.noreply.github.com>
2025-06-03 22:20:33 -0500

    Fix RingCentral SDK import with .SDK

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const SDK = require('@ringcentral/sdk');
+const SDK = require('@ringcentral/sdk').SDK;
 const axios = require('axios');
 
 const app = express();
